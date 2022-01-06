9609b1e45b5db892fe28af4ad67cbe7b21b86591
Rich Palestri <92674403+theUncluded@users.noreply.github.com>
2022-01-06 14:41:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,4 +31,24 @@ menu2.classList.add("main")
 
 //7 Remove the class main on the div with a class of footer.
 
-const 
+menu2.remove()
+
+//8 Create a new li element.
+const menu3 = document.createElement("li")
+
+//9 Give the li the text "four"
+menu3.innerText = "four"
+
+//10 Append the li to the ul element.
+var appendLiUl = document.querySelector("ul")
+appendLiUl.appendChild(menu3)
+
+//11 or 12 Loop over all of the lis inside the ol tag and give them a background color of "green".
+const loopLi = document.querySelectorAll("ol li")
+for(let i =0;i<loopLi.length;i++)
+    loopLi[i].style.backgroundColor = "green"
+
+//13 Remove the div with a class of footer.
+const removeDiv = document.querySelector(".footer")
+removeDiv.remove()
+
